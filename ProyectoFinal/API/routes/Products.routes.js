e0381c1aadc4313a00de8544f973e9e111d3e00c
sofia osorio <david.patino1@utp.edu.co>
2024-06-18 18:00:20 -0500

    Actualizando crud

--- ProyectoFinal/API/routes/Products.routes.js
+++ ProyectoFinal/API/routes/Products.routes.js
@@ -26,6 +26,28 @@ router.post('/', authenticateJWT, async (req, res) => {
   }
 });
 
-// router.put('/', authenticateJWT, async (req, res) =>)
+router.put('/', authenticateJWT, async (req, res) =>{
+  try {
+    const productData = ProductSchema.parse(req.body);
+    const updatedProduct = await Product.update(productData, {
+      where: { id: req.body.id },
+      });
+      res.json(updatedProduct);
+  } catch (err) {
+      console.error(err);
+      res.status(400).json({ message: err.errors });
+  }
+});
+
+router.delete('/', authenticateJWT, async (req, res) => {
+  try {
+    const id = req.query.id;
+    await Product.destroy({ where: { id } });
+    res.json({ message: 'Product deleted successfully' });
+  } catch (err) {
+      console.error(err);
+      res.status(400).json({ message: 'Error deleting product' });
+  }
+});
 
 export default router;

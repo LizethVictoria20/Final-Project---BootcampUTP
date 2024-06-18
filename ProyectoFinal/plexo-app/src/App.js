ea8b4587db406cb9adeb1562b3124baa1790e876
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-18 15:14:06 -0500

    Creando rutas para la navegación

--- ProyectoFinal/plexo-app/src/App.js
+++ ProyectoFinal/plexo-app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Admin from "./components/Administrador";
+import Card from "./components/Card";
+import Catalogo from "./components/CatalogoProductos";
+import Footer from "./components/Footer";
+import Gmail from "./components/Gmail";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import ShoppingCard from "./components/ShoppingCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/gmail" element={<Gmail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/shopping-card" element={<ShoppingCard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ ProyectoFinal/plexo-app/src/components/Gmail/index.js
@@ -0,0 +1,5 @@
+function Gmail() {
+  return <h1>Gmail</h1>;
+}
+
+export default Gmail;

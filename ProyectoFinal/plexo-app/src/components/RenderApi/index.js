76c86afd9c88ae925a03d82d7c747e994bf99b78
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-19 18:58:50 -0500

    Renderizando api de productos

--- ProyectoFinal/plexo-app/src/components/RenderApi/index.js
+++ ProyectoFinal/plexo-app/src/components/RenderApi/index.js
@@ -1,5 +1,16 @@
+import Axios from "axios";
 function RenderAPI(params) {
-  fetch("http://localhost:3000/api/products").then((data) => console.log(data));
+  //GET
+  const getProducts = () => {
+    Axios.get("https://final-project-bootcamputp.onrender.com/api/products")
+      .then((data) => console.log(data.data))
+      .catch((error) => console.log(error));
+  };
+  return(
+    <div>
+      <button onClick={getProducts}>ask</button>
+    </div>
+  )
 }
 
 export default RenderAPI;

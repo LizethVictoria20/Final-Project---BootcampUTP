77001532e565d2c8f14ee048e588258a3e491eb0
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-19 15:34:44 -0500

    diseño inicial

--- ProyectoFinal/plexo-app/src/components/Home/index.js
+++ ProyectoFinal/plexo-app/src/components/Home/index.js
@@ -1,10 +1,21 @@
 import Navbar from "../Navbar/index";
+import Headphone from '../../assets/images/headphones.png'
+import './style.css'
 
 function Home(params) {
   return (
     <>
       <Navbar />
-      <h1>Home</h1>
+      <div className="container-sm container-home rounded-4">
+        <p>Beats Solo</p> 
+        <h1>Wireless</h1>  
+        <h1 className='title-home'>Headphones</h1> 
+        <button className='btn btn-home text-white'>Shop by category</button> 
+        <div className='containerImg'>
+          <img src={Headphone} alt="" />
+        </div>  
+      </div>
+
     </>
   );
 }

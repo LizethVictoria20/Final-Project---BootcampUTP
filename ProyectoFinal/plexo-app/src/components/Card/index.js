62edd1d0d468e927fdd673f48f92eb55c838e0af
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-19 09:33:58 -0500

    Agregando primer diseño de las card

--- ProyectoFinal/plexo-app/src/components/Card/index.js
+++ ProyectoFinal/plexo-app/src/components/Card/index.js
@@ -1,10 +1,67 @@
 import Navbar from "../Navbar/index";
+import Product from "../../assets/images/product.jpg";
+import './style.css'
+import { CiHeart } from "react-icons/ci";
 
-function Card() {
+
+function Card(props) {
   return (
     <>
       <Navbar />
-      <h1>Card</h1>
+      <div className="container mt-5 d-flex justify-content-around">
+      <div className="card rounded-5">
+        <img src={Product} className="card-img-top centered-image rounded-5" alt="..." />
+        <div className="card-body text-dark">
+          <h5 className="card-title">Product name</h5>
+          <p className="card-text">
+            $price
+          </p>
+          <div className="icon-container">
+            <CiHeart color="#7429ba" fontSize="2em" />
+          </div>
+        </div>
+        
+      </div>
+      <div className="card rounded-5">
+        <img src={Product} className="card-img-top rounded-5" alt="..." />
+        <div className="card-body text-dark">
+          <h5 className="card-title">Product name</h5>
+          <p className="card-text">
+          $price
+          </p>
+          <div className="icon-container">
+            <CiHeart color="#7429ba" fontSize="2em" />
+          </div>
+        </div>
+        
+      </div>
+      <div className="card rounded-5">
+        <img src={Product} className="card-img-top rounded-5" alt="..." />
+        <div className="card-body text-dark">
+          <h5 className="card-title">Product name</h5>
+          <p className="card-text">
+          $price
+          </p>
+          <div className="icon-container">
+            <CiHeart color="#7429ba" fontSize="2em" />
+          </div>
+        </div>
+        
+      </div>
+      <div className="card rounded-5">
+        <img src={Product} className="card-img-top rounded-5" alt="..." />
+        <div className="card-body text-dark">
+          <h5 className="card-title">Product name</h5>
+          <p className="card-text">
+          $price
+          </p>
+          <div className="icon-container">
+            <CiHeart color="#7429ba" fontSize="2em" />
+          </div>
+        </div>
+        
+      </div>
+      </div>
     </>
   );
 }

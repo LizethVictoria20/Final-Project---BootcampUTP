638c5737d8316d5557c128b8a4fc6a5448505341
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-19 09:58:51 -0500

    Haciendo cards responsive

--- ProyectoFinal/plexo-app/src/components/Card/index.js
+++ ProyectoFinal/plexo-app/src/components/Card/index.js
@@ -8,62 +8,27 @@ function Card(props) {
   return (
     <>
       <Navbar />
-      <div className="container mt-5 d-flex justify-content-around">
-      <div className="card rounded-5">
-        <img src={Product} className="card-img-top centered-image rounded-5" alt="..." />
-        <div className="card-body text-dark">
-          <h5 className="card-title">Product name</h5>
-          <p className="card-text">
-            $price
-          </p>
-          <div className="icon-container">
-            <CiHeart color="#7429ba" fontSize="2em" />
+      <div className="container text-center mt-5">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+          <div className="col">      
+            <div className="card rounded-5">
+              <img src={Product} className="card-img-top centered-image rounded-5" alt="..." />
+              <div className="card-body text-dark">
+                <h5 className="card-title">Product name</h5>
+                <p className="card-text">$price</p>
+                <div className="icon-container">
+                  <CiHeart color="#7429ba" fontSize="2em" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        
-      </div>
-      <div className="card rounded-5">
-        <img src={Product} className="card-img-top rounded-5" alt="..." />
-        <div className="card-body text-dark">
-          <h5 className="card-title">Product name</h5>
-          <p className="card-text">
-          $price
-          </p>
-          <div className="icon-container">
-            <CiHeart color="#7429ba" fontSize="2em" />
-          </div>
-        </div>
-        
-      </div>
-      <div className="card rounded-5">
-        <img src={Product} className="card-img-top rounded-5" alt="..." />
-        <div className="card-body text-dark">
-          <h5 className="card-title">Product name</h5>
-          <p className="card-text">
-          $price
-          </p>
-          <div className="icon-container">
-            <CiHeart color="#7429ba" fontSize="2em" />
-          </div>
-        </div>
-        
-      </div>
-      <div className="card rounded-5">
-        <img src={Product} className="card-img-top rounded-5" alt="..." />
-        <div className="card-body text-dark">
-          <h5 className="card-title">Product name</h5>
-          <p className="card-text">
-          $price
-          </p>
-          <div className="icon-container">
-            <CiHeart color="#7429ba" fontSize="2em" />
-          </div>
-        </div>
-        
-      </div>
       </div>
     </>
   );
 }
 
 export default Card;
+
+
+

f431d028cf2440e6a87b015a9a7e8cba7ed38abb
andresjararuiz <andresjararuiz01@gmail.com>
2024-06-20 03:13:22 -0500

    MoreEsteticav3

--- ProyectoFinal/plexo-app/src/components/Administrador/index.js
+++ ProyectoFinal/plexo-app/src/components/Administrador/index.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import Navbar from "../Navbar/index";
 import axios from 'axios'
+import pluscircle from "../../assets/images/plus-circle.png"
+import searchcircle from "../../assets/images/search-circle.png"
 import { Add, Login } from "./addProduct";
 import "./stylesheet.css"
 
@@ -10,36 +12,32 @@ function Admin() {
   const UrlPostProducts = 'https://final-project-bootcamputp.onrender.com/api/products'
   const [ productsData, setProductsData] = useState('')
   const UrlProducts = 'https://final-project-bootcamputp.onrender.com/api/products'
-  
+
   const GetApiData = (url) => {
     axios.get(url).then((response) => {
       setProductsData(response.data)
       console.log(productsData);
-      
+
   })
 };
 
 useEffect(() => {
-  GetApiData(UrlProducts) 
+  GetApiData(UrlProducts)
 });
 
   return (
     <>
-    
+
       <Navbar />
       <div className="containerAll">
         <div className="container">
           <div className="containerElements">
                 <div className="productsAddSearch">
-                  <h1>Productos</h1> 
-                  <button className="addItem" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="white" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-                      </svg>
-                      </button>
+                  <h1>Productos</h1>
+                  <button className="addItem" ><img src={pluscircle} alt="plus circle"/></button>
                   <input className="searchBar"></input>
                 </div>
-                <div className="containerProducts">{productsData.map(product => (  
+                <div className="containerProducts">{productsData.map(product => (
                     <div className="product">
                           <img className="img-product" src={product.image_url} alt="img-product"/>
                           <h4>{product.name}</h4>
@@ -53,10 +51,10 @@ useEffect(() => {
             ))}</div>
             </div>
         </div>
-      </div>
+      </div> 
     </>
-      
-      
+
+
   );
 }
 

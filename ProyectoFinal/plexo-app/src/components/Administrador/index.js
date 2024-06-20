47aa128005beea0f232f6c78ca311ada4416cc5e
unknown <santiago.castano@utp.edu.co>
2024-06-19 20:58:45 -0500

    añadiendo productos a la interfaz

--- ProyectoFinal/plexo-app/src/components/Administrador/index.js
+++ ProyectoFinal/plexo-app/src/components/Administrador/index.js
@@ -1,11 +1,44 @@
+import { useState, useEffect } from "react";
 import Navbar from "../Navbar/index";
+import axios from 'axios'
 function Admin() {
+
+  const [ productsData, setProductsData] = useState('')
+  const UrlProducts = 'https://final-project-bootcamputp.onrender.com/api/products'
+  
+  const GetApiData = (url) => {
+    axios.get(url).then((response) => {
+      setProductsData(response.data)
+      console.log(productsData);
+      
+  })
+};
+useEffect(() => {
+   GetApiData(UrlProducts)
+});
+
   return (
+
     <>
       <Navbar />
       <h1>Admin</h1>
+      <div className="container">{productsData.map(product => (
+        <>
+          <div className="product">
+            <img className="img-product" src={product.image_url}></img>
+            <h4>{product.name}</h4>
+            <h4>{product.price}</h4>
+            <p>{product.description}</p>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </>
+
+      ))}</div>
     </>
+    
   );
 }
+
 
 export default Admin;

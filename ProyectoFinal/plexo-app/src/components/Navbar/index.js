ac4410f6131c9b2548e1cfd24d1b22d6429997d4
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-20 23:45:45 -0500

    Agregando link de  nav

--- ProyectoFinal/plexo-app/src/components/Navbar/index.js
+++ ProyectoFinal/plexo-app/src/components/Navbar/index.js
@@ -32,7 +32,9 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="col-7 d-flex justify-content-center align-items-center img-logo">
-          <img src={LogoPurple} alt="Logo" className="navbar-logo" />
+          <Link to="/">
+            <img src={LogoPurple} alt="Logo" className="navbar-logo" />
+          </Link>
         </div>
         <div
           className="collapse navbar-collapse justify-content-end"

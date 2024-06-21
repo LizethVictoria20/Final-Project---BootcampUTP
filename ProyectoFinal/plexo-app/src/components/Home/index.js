2b90fecab0093b62879378fcefee215abe6bb7a9
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-21 01:34:27 -0500

    Ajustado diseño de cards

--- ProyectoFinal/plexo-app/src/components/Home/index.js
+++ ProyectoFinal/plexo-app/src/components/Home/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Navbar from "../Navbar/index";
 import Headphone from '../../assets/images/headphones.png'
 import './style.css'
@@ -9,78 +10,95 @@ import Controller from '../../assets/images/controller.png'
 
 
 
-function Home(params) {
+function Home() {
   return (
     <>
       <Navbar />
-      <div className="sol">
-      <div className="category">
-      <h3>Category</h3>
-        <button> <img src={Clothes} alt="clothes"/> clothes </button> <br/>
-        <button> <img src={Sport} alt="sport"/> sport </button><br/>
-        <button> <img src={Gaming} alt="gaming"/> Clothes </button><br/>
-      </div>
-      <div className="azul">
-      <div className="container-sm container-home rounded-4">
-        <p>Beats Solo</p> 
-        <h1>Wireless</h1>  
-        <h6 className='title-home'>Headphones</h6> 
-        <button className='btn btn-home text-white'>Shop by category</button> 
-        <div className='containerImg'>
-          <img src={Headphone} alt="" />
+        <div className="container-sm container-home rounded-4">
+          <p>Beats Solo</p> 
+          <h1>Wireless</h1>  
+          <h6 className='title-home'>Headphones</h6> 
+          <Link to='catalogo' className='btn btn-home text-white'>Shop by category</Link> 
+          <div className='containerImg'>
+            <img src={Headphone} alt="" />
+            </div>
+        </div>
+        <div className='d-flex flex-row flex-wrap justify-content-center'>
+          <div className="morado">
+            <div className="container-fluid container-morado rounded-4">
+              <p>Beats Solo</p>
+              <h6>Wireless</h6>
+              <h6 className='title-morado'>Headphones</h6>
+              <br /><br />
+              <button className='btn btn-morado text-white'>Shop by category</button>
+              <div className='elecontainerImgc'>
+                <img src={Controller} alt=""  />
+              </div>
+            </div>
+          </div>
+
+          <div className="morado">
+            <div className="container-fluid container-morado rounded-4">
+              <p>Beats Solo</p>
+              <h6>Wireless</h6>
+              <h6 className='title-morado'>Headphones</h6>
+              <br /><br />
+              <button className='btn btn-morado text-white'>Shop by category</button>
+              <div className='elecontainerImgc'>
+                <img src={Controller} alt=""  />
+              </div>
+            </div>
+          </div>
+          <div className="morado-4">
+            <div className="container-fluid container-morado rounded-4">
+              <p>Beats Solo</p>
+              <h6>Wireless</h6>
+              <h6 className='title-morado'>Headphones</h6>
+              <br /><br />
+              <button className='btn  btn-morado-big text-white'>Shop by category</button>
+              <div className='elecontainerImgc-4'>
+                <img src={Controller} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="morado-4">
+            <div className="container-fluid container-morado rounded-4">
+              <p>Beats Solo</p>
+              <h6>Wireless</h6>
+              <h6 className='title-morado'>Headphones</h6>
+              <br /><br />
+              <button className='btn  btn-morado-big text-white'>Shop by category</button>
+              <div className='elecontainerImgc-4'>
+                <img src={Controller} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="morado">
+            <div className="container-fluid container-morado rounded-4">
+              <p>Beats Solo</p>
+              <h6>Wireless</h6>
+              <h6 className='title-morado'>Headphones</h6>
+              <br /><br />
+              <button className='btn btn-morado text-white'>Shop by category</button>
+              <div className='elecontainerImgc'>
+                <img src={Controller} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="morado">
+            <div className="container-fluid container-morado rounded-4">
+              <p>Beats Solo</p>
+              <h6>Wireless</h6>
+              <h6 className='title-morado'>Headphones</h6>
+              <br /><br />
+              <button className='btn btn-morado text-white'>Shop by category</button>
+              <div className='elecontainerImgc'>
+                <img src={Controller} alt="" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="morado">
-  <div className="container-fluid container-morado rounded-4">
-    <p>Beats Solo</p>
-    <h6>Wireless</h6>
-    <h6 className='title-morado'>Headphones</h6>
-    <br /><br />
-    <button className='btn btn-morado text-white'>Shop by category</button>
-    <div className='elecontainerImgc'>
-      <img src={Controller} alt="" style={{ width: '30vh', height: '20vh' }} />
-    </div>
-  </div>
-</div>
-<div className="morado">
-  <div className="container-fluid container-morado rounded-4">
-    <p>Beats Solo</p>
-    <h6>Wireless</h6>
-    <h6 className='title-morado'>Headphones</h6>
-    <br /><br />
-    <button className='btn btn-morado text-white'>Shop by category</button>
-    <div className='elecontainerImgc'>
-      <img src={Controller} alt="" style={{ width: '30vh', height: '20vh' }} />
-    </div>
-  </div>
-</div>
-<div className="morado">
-  <div className="container-fluid container-morado rounded-4">
-    <p>Beats Solo</p>
-    <h6>Wireless</h6>
-    <h6 className='title-morado'>Headphones</h6>
-    <br /><br />
-    <button className='btn btn-morado text-white'>Shop by category</button>
-    <div className='elecontainerImgc'>
-      <img src={Controller} alt="" style={{ width: '30vh', height: '20vh' }} />
-    </div>
-  </div>
-</div>
-<div className="morado">
-  <div className="container-fluid container-morado rounded-4">
-    <p>Beats Solo</p>
-    <h6>Wireless</h6>
-    <h6 className='title-morado'>Headphones</h6>
-    <br /><br />
-    <button className='btn btn-morado text-white'>Shop by category</button>
-    <div className='elecontainerImgc'>
-      <img src={Controller} alt="" style={{ width: '30vh', height: '20vh' }} />
-    </div>
-  </div>
-</div>
-        </div>
-      </div>
-    </>
+      </>
   );
 }
 

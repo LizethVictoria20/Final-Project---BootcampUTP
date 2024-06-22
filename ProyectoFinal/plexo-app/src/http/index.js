963fcfc4ef4d200598d878cbcde8f09f71c9c689
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-21 23:03:11 -0500

    fix: API base URL

--- ProyectoFinal/plexo-app/src/http/index.js
+++ ProyectoFinal/plexo-app/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://final-project-bootcamputp.onrender.com/api/",
   headers: {
         "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE",
         'Access-Control-Allow-Origin': '*',

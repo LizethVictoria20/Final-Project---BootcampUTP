da0956b42d61b332ae2f347073df0e0938c3b182
SMCkillua~ <147349869+SMCkillua@users.noreply.github.com>
2024-06-22 09:03:24 -0500

    cors basado en los cambios de jaguard

--- ProyectoFinal/API/app.js
+++ ProyectoFinal/API/app.js
@@ -1,45 +1,51 @@
 import sequelize from "./config/config.js";
-import { authenticateJWT } from './middleware/jwtMiddleware.js'; 
+import { authenticateJWT } from "./middleware/jwtMiddleware.js";
 import usersRouter from "./routes/Users.routes.js";
 import productsRouter from "./routes/Products.routes.js";
 import categoriesRouter from "./routes/Categories.routes.js";
 import authRouter from "./routes/auth.Routes.js";
-import OrdersRouter from './routes/Orders.Routes.js';
-import CartsRoutes from './routes/Carts.Routes.js';
-import paymentRouter from './routes/payment.routes.js';
+import OrdersRouter from "./routes/Orders.Routes.js";
+import CartsRoutes from "./routes/Carts.Routes.js";
+import paymentRouter from "./routes/payment.routes.js";
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-
-
-const corsOptions = {
-  origin: '*', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, 
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-};
+const whitelist = ["http://localhost:3000", "https://plexoshop.vercel.app"];
 
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    optionsSuccessStatus: 200,
+    credentials: true,
+    methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
+  })
+);
 app.use(express.json());
 
-
 // Rutas de autenticación
-app.use('/api/auth', authRouter); 
-app.use('/api/products', productsRouter);
-app.use('/api/categories', categoriesRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/categories", categoriesRouter);
 
 // Rutas protegidas con JWT
-app.use('/api/payment', paymentRouter);
-app.use('/api/users', authenticateJWT, usersRouter);
-app.use('/api/orders', authenticateJWT, OrdersRouter);
-app.use('/api/carts', authenticateJWT, CartsRoutes);
+app.use("/api/payment", paymentRouter);
+app.use("/api/users", authenticateJWT, usersRouter);
+app.use("/api/orders", authenticateJWT, OrdersRouter);
+app.use("/api/carts", authenticateJWT, CartsRoutes);
 
 app.use((req, res) => {
   res.status(404).json({

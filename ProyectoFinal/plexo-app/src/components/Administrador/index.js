bd1d66d617ee89dc54330d82ce5dca4fbf529412
andresjararuiz <andresjararuiz01@gmail.com>
2024-06-22 19:59:12 -0500

    SearchAdminv1

--- ProyectoFinal/plexo-app/src/components/Administrador/index.js
+++ ProyectoFinal/plexo-app/src/components/Administrador/index.js
@@ -7,6 +7,7 @@ import { IoSearchCircle } from "react-icons/io5";
 import ModalComponentAdd from "./modalAdd";
 import { DeleteProduct } from "./AdminCrud";
 import { FaRegTrashAlt } from "react-icons/fa";
+import SearchProducts from "../Buscador";  // Importar el componente SearchProducts
 
 function Admin() {
   const deleteProduct = DeleteProduct();
@@ -19,11 +20,14 @@ function Admin() {
 
   // get
   const [productsData, setProductsData] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [search, setSearch] = useState("");
   const UrlProducts = 'https://final-project-bootcamputp.onrender.com/api/products';
 
   const GetApiData = (url) => {
     axios.get(url).then((response) => {
       setProductsData(response.data);
+      setFilteredProducts(response.data);  // También actualizar los productos filtrados
     });
   };
 
@@ -36,24 +40,36 @@ function Admin() {
     GetApiData(UrlProducts); 
   };
 
+  const handleSearch = (e) => {
+    const terminoBusqueda = e.target.value;
+    setSearch(terminoBusqueda);
+    const resultadoBusqueda = productsData.filter((product) =>
+      product.name.toLowerCase().includes(terminoBusqueda.toLowerCase())
+    );
+    setFilteredProducts(resultadoBusqueda);
+  };
+
   return (
     <>
       <Navbar />
       <div className="container mt-5">
-        <div className="  p-4 shadow rounded containerAll_admin">
-          <div className="d-flex justify-content-around align-items-center mb-3 " id="d-flex">
+        <div className="p-4 shadow rounded containerAll_admin">
+          <div className="d-flex justify-content-around align-items-center mb-3" id="d-flex">
             <h1 className="h1_admin">Productos</h1>
             <div className="d-flex justify-content-between custom1 productsAddSearch_admin">              
-                <ModalComponentAdd color="red" onProductAdded={handleProductUpdated} />
-              <div className="input-group  ">
-                  <IoSearchCircle size="40px" color="white" className="addItem_admin" />
-                  <input type="search" className=" searchBar_admin" placeholder="Buscar" />
+              <ModalComponentAdd color="red" onProductAdded={handleProductUpdated} />
+              <div className="input-group">
+                <IoSearchCircle size="40px" color="white" className="addItem_admin" />
+                <SearchProducts 
+                  products={productsData}
+                  setFilteredProducts={setFilteredProducts}
+                />
               </div>
             </div>
           </div>
           <div className="list-group">
-            {productsData.map(product => (
-              <div className="list-group-item d-flex justify-content-between align-items-center mb-2 custom-item .container_item productUp_admin" key={product.id}>
+            {filteredProducts.map(product => (
+              <div className="list-group-item d-flex justify-content-between align-items-center mb-2 custom-item productUp_admin" key={product.id}>
                 <div className="d-flex align-items-center">
                   <img src={product.image_url} alt="product" className="rounded-circle me-3" style={{ width: '50px', height: '50px' }} />
                   <div className="d-grid template-columns-3fr product_admin">
@@ -66,8 +82,8 @@ function Admin() {
                   <button className="btn btn-outline me-2">
                     <ModalComponentEdit product={product} onProductUpdated={handleProductUpdated} />
                   </button>
-                  <button className="btn btn-outline-danger " onClick={() => handleDeleteClick(product.product_id)}>
-                  <FaRegTrashAlt />
+                  <button className="btn btn-outline-danger" onClick={() => handleDeleteClick(product.product_id)}>
+                    <FaRegTrashAlt />
                   </button>
                 </div>
               </div>
@@ -78,4 +94,5 @@ function Admin() {
     </>
   );
 }
+
 export default Admin;

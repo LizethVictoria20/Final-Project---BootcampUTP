7babcc3dbc6a0cffe500daddb592f7749f73d4e7
SMCkillua~ <147349869+SMCkillua@users.noreply.github.com>
2024-06-24 06:27:39 -0500

    importacion de branches

--- ProyectoFinal/plexo-app/src/App.js
+++ ProyectoFinal/plexo-app/src/App.js
@@ -14,7 +14,8 @@ import Perfil from "./components/Perfil";
 import NotFound from "./components/Error/NotFound";
 import Shipping from "./components/Shipping";
 import Setting from "./components/AccountSettings/AccountSettings";
-
+import PaymentSuccess from './components/PaymentSuccess/PaymentSuccess.js';
+import PaymentFailed from './components/PaymentFailed/PaymentFailed.js';
 function App() {
   return (
     <Router>
@@ -33,6 +34,8 @@ function App() {
         <Route path="/shipping" element={<Shipping />} />
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/setting" element={<Setting />} />
+        <Route path="/success" element={<PaymentSuccess />} />
+        <Route path="/cancel" element={<PaymentFailed />} />
       </Routes>
     </Router>
   );

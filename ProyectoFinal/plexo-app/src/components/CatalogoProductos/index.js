ec53b292bb81021a79c9475663c3e14f8009a67a
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-26 19:08:41 -0500

    Cambiando posicion de barra de busqueda

--- ProyectoFinal/plexo-app/src/components/CatalogoProductos/index.js
+++ ProyectoFinal/plexo-app/src/components/CatalogoProductos/index.js
@@ -54,6 +54,12 @@ const Catalogo = () => {
   return (
     <>
       <div className="container-fluid text-center container-catalogo">
+        <div className="search-bar">
+          <SearchProducts
+            setFilteredProducts={setFilteredProducts}
+            products={products}
+          />
+        </div>
         <div className="row">
           <div className="col-lg-2 col-md-3 mb-4 text-black">
             <div className="container-categories d-flex flex-column ">
@@ -79,16 +85,13 @@ const Catalogo = () => {
             </div>
           </div>
           <div className="col">
-            <div className="search-bar">
-              <SearchProducts
-                setFilteredProducts={setFilteredProducts}
-                products={products}
-              />
-            </div>
             <div className="container-products d-flex flex-wrap justify-content-center">
               {getFilteredProducts().length > 0 ? (
                 getFilteredProducts().map((product) => (
-                  <Link to={`/product/${product.product_id}`} className="card-title-product">
+                  <Link
+                    to={`/product/${product.product_id}`}
+                    className="card-title-product"
+                  >
                     <Card key={product.product_id} product={product} />
                   </Link>
                 ))

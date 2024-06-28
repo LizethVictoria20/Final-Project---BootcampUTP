dad18b881f171e00c5e012f2acd478df44d3899e
TheYiro <theyerotspace@gmail.com>
2024-06-28 01:10:39 -0500

    BIG UPDATE - CLOUD PREPARATION - SPACE FOR IMAGES FROM FILES - FUNCTIONALITY - Joaco

--- ProyectoFinal/plexo-app/src/components/Administrador/modalAdd.js
+++ ProyectoFinal/plexo-app/src/components/Administrador/modalAdd.js
@@ -8,17 +8,20 @@ function ModalComponentAdd({ onProductAdded }) {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
-  const [image_url, setImage_url] = useState('');
+  const [image_url, setImageUrl] = useState('');
   const [stock, setStock] = useState('');
-  const [category_id, setCategory_id] = useState('');
+  const [category_id, setCategoryId] = useState('');
   const [errors, setErrors] = useState({});
   const [apiError, setApiError] = useState('');
+  const [preview, setPreview] = useState(null);
+  const [dragging, setDragging] = useState(false);
 
   const handleClose = () => {
     setShow(false);
     setErrors({});
     setApiError('');
   };
+
   const handleShow = () => setShow(true);
 
   const validate = () => {
@@ -50,6 +53,38 @@ function ModalComponentAdd({ onProductAdded }) {
     return errors;
   };
 
+  const handleFileChange = (file) => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleInputFileChange = (e) => {
+    const file = e.target.files[0];
+    handleFileChange(file);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+    const file = e.dataTransfer.files[0];
+    handleFileChange(file);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setDragging(false);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const validationErrors = validate();
@@ -78,9 +113,9 @@ function ModalComponentAdd({ onProductAdded }) {
       setName('');
       setPrice('');
       setDescription('');
-      setImage_url('');
+      setImageUrl('');
       setStock('');
-      setCategory_id('');
+      setCategoryId('');
       setErrors({});
       handleClose();
     } catch (error) {
@@ -110,19 +145,6 @@ function ModalComponentAdd({ onProductAdded }) {
               {apiError && <div className="alert alert-danger">{apiError}</div>}
               <form onSubmit={handleSubmit}>
                 <div className="d-flex flex-column align-items-center">
-                  <div className="mb-3 w-100">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="URL de la imagen"
-                      value={image_url}
-                      onChange={(e) => setImage_url(e.target.value)}
-                      required
-                    />
-                    {errors.image_url && (
-                      <div className="text-danger">{errors.image_url}</div>
-                    )}
-                  </div>
                   <div className="mb-3 w-100">
                     <input
                       type="text"
@@ -168,7 +190,7 @@ function ModalComponentAdd({ onProductAdded }) {
                       className="form-control"
                       placeholder="ID de la categoría"
                       value={category_id}
-                      onChange={(e) => setCategory_id(e.target.value)}
+                      onChange={(e) => setCategoryId(e.target.value)}
                       required
                     />
                     {errors.category_id && (
@@ -186,6 +208,40 @@ function ModalComponentAdd({ onProductAdded }) {
                     />
                     {errors.description && (
                       <div className="text-danger">{errors.description}</div>
+                    )}
+                  </div>
+                  <div
+                    className={`upload-button-container mb-3 w-100 ${dragging ? 'dragging' : ''}`}
+                    onDragOver={handleDragOver}
+                    onDragLeave={handleDragLeave}
+                    onDrop={handleDrop}
+                  >
+                    <label htmlFor="file-upload" className="upload-label">
+                      <div className="upload-design">
+                        {preview ? (
+                          <img src={preview} alt="Preview" className="preview-image" />
+                        ) : (
+                          <>
+                            <svg viewBox="0 0 640 512" className="upload-icon">
+                              {/* <path
+                                d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
+                              /> */}
+                            </svg>
+                            <p>Drag and Drop</p>
+                            <p>or</p>
+                            <span className="browse-button">Browse file</span>
+                          </>
+                        )}
+                      </div>
+                      <input
+                        id="file-upload"
+                        type="file"
+                        className="file-input"
+                        onChange={handleInputFileChange}
+                      />
+                    </label>
+                    {errors.image_url && (
+                      <div className="text-danger">{errors.image_url}</div>
                     )}
                   </div>
                   <button type="submit" className="btn btn-primary btn-purple text-white">Agregar Producto</button>
--- ProyectoFinal/plexo-app/src/components/Administrador/modalEdit.js
+++ ProyectoFinal/plexo-app/src/components/Administrador/modalEdit.js
@@ -12,9 +12,10 @@ function ModalComponentEdit({ product, onProductUpdated }) {
   const [stock, setStock] = useState('');
   const [category_id, setCategoryId] = useState('');
   const [errors, setErrors] = useState({});
+  const [preview, setPreview] = useState(null);
+  const [dragging, setDragging] = useState(false);
 
   useEffect(() => {
-    console.log(product);
     if (product) {
       setName(product.name);
       setPrice(product.price.toString());
@@ -22,6 +23,7 @@ function ModalComponentEdit({ product, onProductUpdated }) {
       setImageUrl(product.image_url);
       setStock(product.stock.toString());
       setCategoryId(product.category_id.toString());
+      setPreview(product.image_url); // Inicializar la vista previa de la imagen
     }
   }, [product]);
 
@@ -41,6 +43,38 @@ function ModalComponentEdit({ product, onProductUpdated }) {
     if (!stock || isNaN(stock)) newErrors.stock = 'Debe ser un número válido.';
     if (!category_id || isNaN(category_id)) newErrors.category_id = 'Debe ser un número válido.';
     return newErrors;
+  };
+
+  const handleFileChange = (file) => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleInputFileChange = (e) => {
+    const file = e.target.files[0];
+    handleFileChange(file);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+    const file = e.dataTransfer.files[0];
+    handleFileChange(file);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setDragging(false);
   };
 
   const handleSubmit = async (event) => {
@@ -90,16 +124,7 @@ function ModalComponentEdit({ product, onProductUpdated }) {
               {errors.general && <div className="alert alert-danger">{errors.general}</div>}
               <form onSubmit={handleSubmit}>
                 <div className="d-flex flex-column align-items-center">
-                  <div className="mb-3 w-100 text-center">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="URL"
-                      value={image_url}
-                      onChange={(e) => setImageUrl(e.target.value)}
-                    />
-                    {errors.image_url && <div className="text-danger">{errors.image_url}</div>}
-                  </div>
+                  
                   <div className="mb-3 w-100">
                     <input
                       type="text"
@@ -150,6 +175,40 @@ function ModalComponentEdit({ product, onProductUpdated }) {
                     />
                     {errors.category_id && <div className="text-danger">{errors.category_id}</div>}
                   </div>
+                  
+                  <div
+                    className={`upload-button-container mb-3 w-100 ${dragging ? 'dragging' : ''}`}
+                    onDragOver={handleDragOver}
+                    onDragLeave={handleDragLeave}
+                    onDrop={handleDrop}
+                  >
+                    <label htmlFor="file-upload" className="upload-label">
+                      <div className="upload-design">
+                        {preview ? (
+                          <img src={preview} alt="Preview" className="preview-image" />
+                        ) : (
+                          <>
+                            <svg viewBox="0 0 640 512" className="upload-icon">
+                              <path
+                                d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
+                              ></path>
+                            </svg>
+                            <p>Drag and Drop</p>
+                            <p>or</p>
+                            <span className="browse-button">Browse file</span>
+                          </>
+                        )}
+                      </div>
+                      <input
+                        id="file-upload"
+                        type="file"
+                        className="file-input"
+                        onChange={handleInputFileChange}
+                      />
+                    </label>
+                    {errors.image_url && <div className="text-danger">{errors.image_url}</div>}
+                  </div>
+                  
                   <button type="submit" className="btn btn-primary">Actualizar Producto</button>
                 </div>
               </form>

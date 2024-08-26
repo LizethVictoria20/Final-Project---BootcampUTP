efb5e6a8a94545ee32135418f7a695bcf37f011f
LizethVictoria20 <lizvictoria754@gmail.com>
2024-08-26 13:42:43 -0500

    Aliminando carpetas no necesarias

--- ProyectoFinal/API/schemas/CategorySchema.js
+++
@@ -1,8 +0,0 @@
-import z from 'zod';
-
-const CategorySchema = z.object({
-  name: z.string().min(3).max(100),
-  description: z.string().min(10).max(1000).optional(),
-});
-
-export default CategorySchema;

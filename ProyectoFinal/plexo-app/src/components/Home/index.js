8aad889a5bcccf51208e9da907acec403b9d8cc2
LizethVictoria20 <lizvictoria754@gmail.com>
2024-09-02 14:35:54 -0500

    Agregando nuevo header

--- ProyectoFinal/plexo-app/src/components/Home/index.js
+++ ProyectoFinal/plexo-app/src/components/Home/index.js
@@ -1,53 +1,35 @@
 import { Link } from "react-router-dom";
 import "./style-home.css";
-import Home1 from "../../assets/images/home1.png";
-import Home2 from "../../assets/images/home2.png";
-import Product from "../../assets/images/product.png";
-import Product1 from "../../assets/images/product1.png";
-import Product2 from "../../assets/images/product2.png";
+
 
 function Home() {
   return (
     <>
-      <div className="container-sm container-home1 rounded-4 mb-4">
-        <Link to="/catalogoPlexo">
-          <img src={Home1} alt="" />
-        </Link>
-      </div>
-      <div>
-        <h1 className="text-center fw-bold mb-4 text-white">
-          Best Seller Products
-        </h1>
-      </div>
-      <div className="container text-center container-best_seller d-flex flex-wrap justify-content-center">
-        <div className="row">
-          <div className="col col-catalogo">
-            <Link to="https://plexoshop.vercel.app/product/195">
-              <img src={Product} alt="" />
-            </Link>
-          </div>
-          <div className="col col-catalogo">
-            <Link to="https://plexoshop.vercel.app/product/196">
-              <img src={Product1} alt="" />
-            </Link>{" "}
-          </div>
-          <div className="col col-catalogo">
-            <Link to="https://plexoshop.vercel.app/product/194">
-              <img src={Product2} alt="" />
-            </Link>
-          </div>
-          <div className="col col-catalogo">
-            <Link to="https://plexoshop.vercel.app/product/195">
-              <img src={Product} alt="" />
-            </Link>
+      <div className="hero-banner-container">
+        <div>
+          <p className="beats-solo">Beats Solo Air</p>
+          <h3>Summer Sale</h3>
+          <h1>FINE</h1>
+          <img src="https://cdn.sanity.io/images/vfxfwnaw/production/a64b345016e96adfb8849af5521c8e0ecfe8f027-555x555.webp" alt="headphones" className="hero-banner-image" />
+          <div>
+            <button type="button">Shop Now</button>
           </div>
         </div>
       </div>
-      <div className="container-sm container-home rounded-4 mb-4">
-        <Link to="/catalogoPlexo">
-          <img src={Home2} alt="" />
-        </Link>
-      </div>
+
+      {/* Best seller */}
+      <div>
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
+
+    <div className="products-container">
+      {/* {products?.map((product) => <Product key={product._id} product={product} />)} */}
+      <h1>products </h1>
+    </div>
+
+  </div>
     </>
   );
 }

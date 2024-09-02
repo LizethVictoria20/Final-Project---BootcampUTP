1db2981f43367b57a75c4bbc909813b54b0e1438
LizethVictoria20 <lizvictoria754@gmail.com>
2024-09-02 15:23:15 -0500

    Agregando productos del header

--- ProyectoFinal/plexo-app/src/components/Home/index.js
+++ ProyectoFinal/plexo-app/src/components/Home/index.js
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
 import "./style-home.css";
-
+import { useState, useEffect } from "react";
+import api from "../../http/index";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get("/products").then((response) => {
+      const allProducts = response.data;
+      setProducts(allProducts)
+    });
+  }, []);
   return (
     <>
       <div className="hero-banner-container">
@@ -10,7 +19,11 @@ function Home() {
           <p className="beats-solo">Beats Solo Air</p>
           <h3>Summer Sale</h3>
           <h1>FINE</h1>
-          <img src="https://cdn.sanity.io/images/vfxfwnaw/production/a64b345016e96adfb8849af5521c8e0ecfe8f027-555x555.webp" alt="headphones" className="hero-banner-image" />
+          <img
+            src="https://cdn.sanity.io/images/vfxfwnaw/production/a64b345016e96adfb8849af5521c8e0ecfe8f027-555x555.webp"
+            alt="headphones"
+            className="hero-banner-image"
+          />
           <div>
             <button type="button">Shop Now</button>
           </div>
@@ -19,17 +32,29 @@ function Home() {
 
       {/* Best seller */}
       <div>
-    <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
-    </div>
+        <div className="products-heading">
+          <h2>Best Seller Products</h2>
+          <p>speaker There are many variations passages</p>
+        </div>
 
-    <div className="products-container">
-      {/* {products?.map((product) => <Product key={product._id} product={product} />)} */}
-      <h1>products </h1>
-    </div>
-
-  </div>
+        <div className="products-container">
+          {products?.slice(0, 4).map((product) => 
+            <>
+              <div className="product-card" key={product.product_id}>
+                <img 
+                src={product.image_url}
+                
+                className="product-image_home"
+                alt={product.name}
+                />
+                <p className="product-name_header">{product.name}</p>
+                <p className="product-price">${product.price}</p>
+              </div>
+            </>
+          )}
+         
+        </div>
+      </div>
     </>
   );
 }
